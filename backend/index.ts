9f372f23e0843a502d379debb0d11910f39bce59
Linda Jensen <linda.jensen81@me.com>
2025-10-23 09:33:48 +0200

    Update CORS for production

--- backend/index.ts
+++ backend/index.ts
@@ -15,10 +15,7 @@ app.use(express.json());
 
 app.use(
 	cors({
-		origin: [
-			"https://devacademy-backend.onrender.com",
-			"http://localhost:5173"
-		],
+		origin: ["http://localhost:5173"],
 		credentials: true
 	})
 );
